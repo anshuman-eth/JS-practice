6ef0a3d3b1b6395343c324cb373110e6adb8947e
Anshuman Singh4 <AS00844638@techmahindra.com>
2022-06-01 01:51:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.time('your code took');
+console.log('Hello World!');
+console.log(4+11);
+console.log(33);
+console.log(true);
+console.log([22,3,44,2,344]);
+console.log({anshuman:'developer', age:34});   //js object
+console.table({anshuman:'developer', age:34});
+console.warn("This is a warning.");
+// console.clear();
+console.timeEnd('your code took');
+console.assert(122>180, 'Wrong age input');
+console.error('Wrong age input');
+ /* This
+ is a multiline comment.
+ */
